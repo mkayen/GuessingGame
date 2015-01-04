0723b405ecb977ef7ce895097aa7973205e9b90d
Max Kayen <mkayen@gmail.com>
2015-01-04 02:37:44 -0500

    Generating Random Numbers & Guesses

--- application.js
+++ application.js
@@ -1,12 +1,36 @@
-// myFunction
-//		1) Generate Random Number onLoad of Page
-//		2) Insert random number into num Array, and refer to this for guessing game.
-//      3) Print Number onLoad of Page (FOR TESTING PURPOSES ONLY)
+// GLOBAL VARIABLES
 
-function randomNumGen() {
-   var element =  document.getElementById("demo")
-   element.innerHTML = Math.floor((Math.random() * 100) + 1);
-   num = []
-   num.push[element.innerHTML]
-   num
-}
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+
+
+// JQUERY EVENT HANDLERS ------------------------------
+
+$(document).ready(function(){
+	
+	// EH1 - Display random number within testNumber (HTML ID)
+		$('.btn-primary').on('click', randomNumGen);
+
+	// EH2 - Retrieve value of input once guess is pressed.
+		$('.btn-primary').on('click', readTheHand);
+
+});
+
+// JAVASCRIPT & JQUERY FUNCTIONS -----------------------
+
+	// randomNumGen: 	Generates Random Number, plugs number into array
+	//					where it will be referenced.
+
+	function randomNumGen(){
+		$('#testingArea').text(randomNumber)
+	}
+
+	// readTheHand: 	Retrieving User Input, and displaying for testing
+	//					purposes.
+
+	function readTheHand(){
+		var guess = +$('#numberForm').val();
+		$('#testingArea').append('<br>Guess equals '+guess+'.<br>');
+	}
+
+	// guessNumber: 	User types number into textbox. Initially checks
+	//					if number is correct or not.
---
+++ jQueryCustom.js
@@ -0,0 +1,7 @@
+<script>
+    $(document).ready(function(){
+      	$('button').on('click',function(){
+        	$('p').append('Son of a Bitch');
+	    });
+	});
+</script>
