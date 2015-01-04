032ea40664da1116f543b4689f9ba908cffe35b5
Max Kayen <mkayen@gmail.com>
2015-01-04 03:28:41 -0500

    Made additions to the attempt1 function, works but needs cleaning.

--- application.js
+++ application.js
@@ -1,27 +1,40 @@
 // GLOBAL VARIABLES
 
 var randomNumber = Math.floor((Math.random() * 100) + 1);
-
+var gameArray = []
+	// in game array,[0] is the random gen, [1] is the guess1
+	// further guesses follow this format.
 
 // JQUERY EVENT HANDLERS ------------------------------
 
 $(document).ready(function(){
 	
 	// EH1 - Display random number within testNumber (HTML ID)
-		$('.btn-primary').on('click', randomNumGen);
+	//	$('.btn-primary').on('load', randomNumGen);
 
 	// EH2 - Retrieve value of input once guess is pressed.
 		$('.btn-primary').on('click', readTheHand);
+
+	// EH3 - Checks guess & randomNumber for a match.
+		$('.btn-primary').on('click', attempt1);
 
 });
 
 // JAVASCRIPT & JQUERY FUNCTIONS -----------------------
 
+	// logFunction: 	For Debugging Purposes, creating basic logging
+	//					In the testingArea.
+
+	function logFunction(result){
+		$('#testingArea').append(result);
+	}
+
 	// randomNumGen: 	Generates Random Number, plugs number into array
 	//					where it will be referenced.
 
 	function randomNumGen(){
-		$('#testingArea').text(randomNumber)
+		gameArray.push(randomNumber);
+		$('#testingArea').text('Random Number equals '+randomNumber+'.');
 	}
 
 	// readTheHand: 	Retrieving User Input, and displaying for testing
@@ -29,8 +42,33 @@ $(document).ready(function(){
 
 	function readTheHand(){
 		var guess = +$('#numberForm').val();
-		$('#testingArea').append('<br>Guess equals '+guess+'.<br>');
+		gameArray.push(guess)
+		logFunction('<br>Guess equals '+guess+'.<br>');
 	}
 
-	// guessNumber: 	User types number into textbox. Initially checks
-	//					if number is correct or not.
+	// attempt1: 		if guess & randomNumber are equal, then the user
+	//					wins. Otherwise, the user loses.
+
+	function attempt1(){
+		$('#testingArea').append('Array: '+gameArray+'<br>');
+		$('#testingArea').append('Random Number: '+gameArray[0]+'<br>');
+		$('#testingArea').append('Guess1: '+gameArray[1]+'<br>');
+		if(gameArray[0] === gameArray[1]){
+			$('#result').text('Guess is Correct!')
+		}else{
+			$('#result').text('Guess is Incorrect. Keep trying, or maybe a hint can help!')
+			if(gameArray[0] === gameArray[2]){
+				$('#result').text('Guess is Correct!')
+			}else{
+				$('#result').text('Guess is Incorrect. Keep trying, or maybe a hint can help!')
+			}
+		}
+
+	// attempt2: 		Try Again
+	
+	function attempt2(){
+
+	}
+
+
+	}
